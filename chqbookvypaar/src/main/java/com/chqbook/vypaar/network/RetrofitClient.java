968d882957b19cc7127acb82fee710b5c11c5b49
pm-chqbook <62986574+pm-chqbook@users.noreply.github.com>
2021-01-07 17:32:07 +0530

    -uat setting

--- chqbookvypaar/src/main/java/com/chqbook/vypaar/network/RetrofitClient.java
+++ chqbookvypaar/src/main/java/com/chqbook/vypaar/network/RetrofitClient.java
@@ -6,7 +6,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static final String BASE_URL = "https://www.chqbook.com/api/";
+    private static final String BASE_URL = "https://uat.chqbook.com/api/";
     private static RetrofitClient mInstance;
     private Retrofit retrofit;
 
